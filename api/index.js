662ea209bc22cb70b2b2b82e144be94c352cdc71
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2023-12-28 11:55:22 +0530

    some ui bugs fixed

--- api/index.js
+++ api/index.js
@@ -51,6 +51,6 @@ app.use("/api/catagories", catagoriesRoute);
 app.listen("3003", () => {
   console.log("running nodejs http://localhost:3003");
 });
-app.get("/",(req,res)=>{
-  res.send("HI");
+app.get("/", (req, res) => {
+  res.send("Working fine");
 })
