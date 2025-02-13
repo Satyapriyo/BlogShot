9f6118c1ae2b8c520a45f91f544c400a0dde91c8
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2025-02-13 08:36:31 +0530

    added some padding in posts section

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -3,7 +3,7 @@ import Post from "../post/Post";
 import Blogs from "../Blogs/Blogs";
 const Posts = ({ posts }) => {
   return (
-    <div className="posts pt-20 text-black">
+    <div className="posts pt-20 text-black pb-4">
       <h1 className="text-5xl max-h-20 md:text-6xl font-bold fontnew text-center mb-8">All Blogs</h1>
       <div className="relative items-center w-full px-5 mx-auto md:px-12 lg:px-24 max-w-7xl">
         <div className="grid w-full grid-cols-1 gap-6 mx-auto">
