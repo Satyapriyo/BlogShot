ea12ab2d6165ce912ee8dff8c1ef715b468fa797
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2025-05-26 21:22:17 +0530

    removed helmet from header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,13 +5,7 @@ import { Helmet } from "react-helmet";
 const Header = () => {
   return (
     <>
-      <Helmet>
-        <link
-          rel="preload"
-          as="image"
-          href="https://res.cloudinary.com/duyocx2j0/image/upload/v1748272326/blue-surface-with-study-tools-min_yfzak1.jpg"
-        />
-      </Helmet>
+     
       <div className="header bg-white">
         <img
           className="headerImg -mt-20 "
